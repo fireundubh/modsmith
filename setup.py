e5f06df88c24075993f37618338f3ffc883a89e0
fireundubh <fireundubh@gmail.com>
2019-01-05 06:14:18 -0800

    Update build script and version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from cx_Freeze import Executable, setup
 
 # Dependencies are automatically detected, but it might need fine tuning.
 build_exe_options = {
-    'packages': ['lxml.etree', 'lxml._elementpath'],
+    'packages': ['configparser', 'functools', 'glob', 'lxml.etree', 'lxml._elementpath', 'operator', 'os', 're', 'traceback', 'types', 'zipfile'],
     'optimize': 2,
     'include_msvcr': False,
     'zip_include_packages': '*',
@@ -12,7 +12,7 @@ build_exe_options = {
 }
 
 setup(name='Modsmith',
-      version='0.1.6',
+      version='0.1.7',
       description='Automatically packages Kingdom Come: Deliverance mods for distribution',
       options={'build_exe': build_exe_options},
       executables=[Executable('__main__.py', base=None, targetName='modsmith.exe')])
