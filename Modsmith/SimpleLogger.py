fb49171f57622776ed71f7aa329769ff9f8d0f0b
fireundubh <fireundubh@gmail.com>
2019-07-22 09:37:50 -0700

    added conditional debug logging

--- Modsmith/SimpleLogger.py
+++ Modsmith/SimpleLogger.py
@@ -1,5 +1,8 @@
 import colorama
+import sys
 from colorama import Fore
+
+from Modsmith.Constants import PRODUCTION
 
 
 class SimpleLogger:
@@ -7,13 +10,18 @@ class SimpleLogger:
         colorama.init()
 
     @staticmethod
-    def error(message: str, prefix: str = '', suffix: str = '') -> None:
+    def error(message: str, *, prefix: str = '', suffix: str = '') -> None:
         print('%s%s[%s] %s%s%s' % (Fore.RED, prefix, 'ERRO', message, suffix, Fore.RESET))
 
     @staticmethod
-    def info(message: str, prefix: str = '', suffix: str = '') -> None:
+    def info(message: str, *, prefix: str = '', suffix: str = '') -> None:
         print('%s[%s] %s%s' % (prefix, 'INFO', message, suffix))
 
     @staticmethod
-    def warn(message: str, prefix: str = '', suffix: str = '') -> None:
+    def warn(message: str, *, prefix: str = '', suffix: str = '') -> None:
         print('%s%s[%s] %s%s%s' % (Fore.YELLOW, prefix, 'WARN', message, suffix, Fore.RESET))
+
+    @staticmethod
+    def debug(message: str, *, prefix: str = '', suffix: str = '') -> None:
+        if not PRODUCTION or PRODUCTION and '--debug' in sys.argv:
+            print('%s%s[%s] %s%s%s' % (Fore.CYAN, prefix, 'DEBUG', message, suffix, Fore.RESET))
